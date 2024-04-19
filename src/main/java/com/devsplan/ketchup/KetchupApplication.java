afb028174d0b979f5592083b51916eab0d7a6830
daeun100299 <goode439767@gmail.com>
2024-04-19 18:13:26 +0900

    template 생성

---
+++ src/main/java/com/devsplan/ketchup/KetchupApplication.java
@@ -0,0 +1,13 @@
+package com.devsplan.ketchup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetchupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KetchupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsplan/ketchup/KetchupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsplan.ketchup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetchupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
