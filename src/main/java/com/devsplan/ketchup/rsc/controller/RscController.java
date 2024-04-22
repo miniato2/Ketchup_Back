dc75acca4f4d9975deaeb6be6498d71780a4e22e
daeun100299 <goode439767@gmail.com>
2024-04-22 15:58:30 +0900

    메일, 자원 dto entity

---
+++ src/main/java/com/devsplan/ketchup/rsc/controller/RscController.java
@@ -0,0 +1,9 @@
+package com.devsplan.ketchup.rsc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rsc")
+public class RscController {
+}
---
+++ src/main/java/com/devsplan/ketchup/rsc/service/RscService.java
@@ -0,0 +1,7 @@
+package com.devsplan.ketchup.rsc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RscService {
+}
