30f1ab9e716cdd12fc2d19d29c1c16a3a1b3ca69
jaylee <jayginwoolee98@gmail.com>
2024-04-22 17:03:29 +0900

    일정 테스트파일 생성

---
+++ src/test/java/com/devsplan/ketchup/schedule/ScheduleRestTests.java
@@ -0,0 +1,81 @@
+package com.devsplan.ketchup.schedule;
+
+import com.devsplan.ketchup.schedule.service.ScheduleService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ScheduleRestTests {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @DisplayName("부서별 일정 목록 조회")
+    @Test
+    void selectScheduleList() {
+        // /department/{departmentno}/schedules?category={filtercategory}
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("부서별 일정 상세 조회")
+    @Test
+    void selectScheduleDetail() {
+        // /department/{departmentno}/schedules/{scheduleno}
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("부서별 일정 등록")
+    @Test
+    void insertSchedule() {
+        // /members
+
+        // given
+
+        // when
+
+        // then
+    }
+
+
+    @DisplayName("부서별 일정 수정")
+    @Test
+    void updateSchedule() {
+        // /schedules/{scheduleno}
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("부서별 일정 삭제")
+    @Test
+    void deleteSchedule() {
+        // /schedules/{scheduleno}
+
+        // given
+
+        // when
+
+        // then
+    }
+
+}
