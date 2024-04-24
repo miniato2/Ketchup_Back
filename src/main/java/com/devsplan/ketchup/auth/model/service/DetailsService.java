81984c7c0a74851f08bd39f7b9883800aa34972b
2eehy <yihooyung@gmail.com>
2024-04-24 16:49:11 +0900

    [Feature-0424](#25) - member - ENTITY 수정

---
+++ src/main/java/com/devsplan/ketchup/auth/model/service/DetailsService.java
@@ -0,0 +1,42 @@
+//package com.devsplan.ketchup.auth.model.service;
+//
+//import com.ohgiraffers.jwtsecurity.auth.model.DetailsUser;
+//import com.devsplan.ketchup.member.service.MemberService;
+//import org.springframework.security.authentication.AuthenticationServiceException;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//public class DetailsService implements UserDetailsService {
+//
+//    private final MemberService memberService;
+//
+//    public DetailsService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+//
+//    /**
+//     * description. 로그인 요청 시 사용자의 id를 받아 DB에서 사용자 정보를 가져오는 메소드
+//     *
+//     * @param username the username identifying the user whose data is required.
+//     * @return UserDetails
+//     * @throws UsernameNotFoundException
+//     */
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        if (username == null || username.equals("")) {
+//            throw new AuthenticationServiceException(username + " is Empty!");
+//
+//        } else {
+//            return userService.findUser(username)
+//                            .map(data -> new DetailsUser(Optional.of(data)))  //널이아닌값을 가지는 객체를 반환한다. 그 정보로 DetailsUser 만들자
+//                            .orElseThrow(() -> new AuthenticationServiceException(username)); // 그과정에서 에러 발생하면 저 예외 던지고 메세지는 username으로 설정
+//        }
+//
+//
+//    }
+//}
--- src/main/java/com/devsplan/ketchup/member/service/MemberService.java
+++ src/main/java/com/devsplan/ketchup/member/service/MemberService.java
@@ -1,4 +1,7 @@
 package com.devsplan.ketchup.member.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
 }
