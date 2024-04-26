523e06619778b7eb96d607dbac1e114f7bfd0eaa
daeun100299 <153487628+daeun100299@users.noreply.github.com>
2024-04-26 13:20:54 +0900

    Revert "[Feature-0426](#43 #45) - 메일 - 발송 취소, 메일 삭제"

--- src/main/java/com/devsplan/ketchup/mail/repository/ReceiverRepository.java
+++ src/main/java/com/devsplan/ketchup/mail/repository/ReceiverRepository.java
@@ -3,8 +3,6 @@ package com.devsplan.ketchup.mail.repository;
 import com.devsplan.ketchup.mail.dto.ReceiverDTO;
 import com.devsplan.ketchup.mail.entity.Receiver;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,11 +10,4 @@ import java.util.List;
 @Repository
 public interface ReceiverRepository  extends JpaRepository<Receiver, Integer> {
     List<Receiver> findByReceiverMem(int receiverTest);
-
-    @Query("SELECT r FROM Receiver r WHERE r.mailNo = :mailNo")
-    List<Receiver> findReadTime(int mailNo);
-
-    @Modifying
-    @Query("UPDATE Receiver r SET r.receiverDelStatus = 'Y' WHERE r.mailNo = :mailNo and r.receiverMem = :receiverMem")
-    int updateByReceiverDelStatus(int mailNo, int receiverMem);
 }
