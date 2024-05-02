aae424bfccfd330a3f372bc365ee7a9a897dd653
miniato2 <whgg456@gmail.com>
2024-05-02 11:58:40 +0900

    240502 11:58 seung commit

--- src/main/java/com/devsplan/ketchup/approval/controller/ApprovalController.java
+++ src/main/java/com/devsplan/ketchup/approval/controller/ApprovalController.java
@@ -1,12 +1,13 @@
 package com.devsplan.ketchup.approval.controller;
 
-import com.devsplan.ketchup.approval.dto.AppLineDTO;
-import com.devsplan.ketchup.approval.dto.ApprovalDTO;
-import com.devsplan.ketchup.approval.dto.RefLineDTO;
+import com.devsplan.ketchup.approval.dto.*;
 import com.devsplan.ketchup.approval.service.ApprovalService;
+import com.devsplan.ketchup.common.Pagenation;
+import com.devsplan.ketchup.common.PagingButton;
 import com.devsplan.ketchup.common.ResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,19 +29,23 @@ public class ApprovalController {
 
     //기안 등록
     @PostMapping("/approvals")
-    public ResponseEntity<ResponseDTO> insertApproval(@ModelAttribute ApprovalDTO approvalDTO,
-                                                      @ModelAttribute List<AppLineDTO> appLineDTOList,
-                                                      @ModelAttribute List<RefLineDTO> refLineDTOList,
-                                                      @RequestParam("multipartFileList") List<MultipartFile> multipartFileList){
+    public ResponseEntity<ResponseDTO> insertApproval(@ModelAttribute AppInputDTO appInputDTO,
+                                                      List<MultipartFile> multipartFileList){
+
+        log.info("==================================");
+        log.info(appInputDTO.toString());
 
         return ResponseEntity.ok().body(
                 new ResponseDTO(HttpStatus.OK, "기안상신 완료",
-                        approvalService.insertApproval(approvalDTO, appLineDTOList, refLineDTOList, multipartFileList))
+                        approvalService.insertApproval(appInputDTO, multipartFileList))
         );
     }
 
     @GetMapping("/approvals")
     public List<ApprovalDTO> selectApprovalList(){
+        Page<ApprovalSelectDTO> appList = null;
+        PagingButton paging = null;
+
         // 검색 결과
         /*switch (category){
             case 1:
@@ -52,16 +57,41 @@ public class ApprovalController {
                 approvalService.selectMyApproval(member, status, search);
                 break;
             case 3:
-                if(status == 전체){ status = 대기, 진행 )
+                if(status == 전체){ status = 대기, 진행 }
                 approvalService.selectReceiveApp(); //
                 break;
             case 4:
                 status = 완료
                 approvalService.selectRefApp(); //상태 없음 => 무조건 승인된건만
                 break;
-
         }*/
         return null;
 
+    }
+
+    //양식 조회
+    @GetMapping("/forms/{formNo}")
+    public ResponseEntity<ResponseDTO> selectForm(@PathVariable int formNo){
+
+        return ResponseEntity.ok().body(
+                new ResponseDTO(HttpStatus.OK, "양식조회 성공",
+                        approvalService.selectForm(formNo))
+        );
+    }
+
+    //기안 상태 수정
+    @PutMapping("/approvals/{approvalNo}")
+    public ResponseEntity<ResponseDTO> updateApprovals(@PathVariable int approvalNo,
+                                                       @RequestBody AppUpdateDTO appUpdateDTO){
+        String result = "";
+        String memberNo = "1";
+
+        if(appUpdateDTO.getAction().equals("회수")){
+            result = approvalService.updateApproval(approvalNo);
+        }else{
+            result = approvalService.updateApproval2(appUpdateDTO, memberNo, approvalNo);
+        }
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "성공", result));
     }
 }
---
+++ src/main/java/com/devsplan/ketchup/approval/entity/AppMember.java
@@ -0,0 +1,7 @@
+package com.devsplan.ketchup.approval.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+public class AppMember {
+}
